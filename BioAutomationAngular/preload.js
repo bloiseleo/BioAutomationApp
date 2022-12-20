fb133e166158abd81f86a157d3c882fad45ba3f5
Leonardo C. S. Bloise <leonardocsbloise@gmail.com>
2022-12-20 11:41:41 -0300

    feat: putting it on renderer process

--- BioAutomationAngular/preload.js
+++ BioAutomationAngular/preload.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require("electron")
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  getExtraResourcesPath: () => ipcRenderer.invoke("get:extraResourcesPath")
+  getExtraResourcesPath: () => ipcRenderer.invoke("get:extraResourcesPath"),
+  uploadDocument: (workspaceName, file, refseq) => ipcRenderer.invoke("make:uploadDocument", {workspaceName, file, refseq})
 })
